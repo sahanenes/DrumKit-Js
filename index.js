97362b7d5a78d00daaef12133973ceb3df878c06
sahanenes <sahanenesss@gmail.com>
2022-12-19 16:51:33 +0300

    event listeners

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-document.querySelector("button");
-
-function handleClick() {
-  alert("i got clicked");
+let numberOfButtons = document.querySelectorAll(".drum");
+for (let i = 0; i < numberOfButtons.length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    if (this.innerHTML == "w") {
+      new Audio("../sounds/tom-1.mp3").play();
+    } else if (this.innerHTML == "a") {
+      new Audio("../sounds/tom-2.mp3").play();
+    } else if (this.innerHTML == "s") {
+      new Audio("../sounds/tom-3.mp3").play();
+    } else if (this.innerHTML == "d") {
+      new Audio("../sounds/tom-4.mp3").play();
+    } else if (this.innerHTML == "j") {
+      new Audio("../sounds/snare.mp3").play();
+    } else if (this.innerHTML == "k") {
+      new Audio("../sounds/crash.mp3").play();
+    } else if (this.innerHTML == "l") {
+      new Audio("../sounds/kick-bass.mp3").play();
+    }
+  });
 }
+document.addEventListener("keydown", (e) => {
+  if (e.key == "w") {
+    new Audio("../sounds/tom-1.mp3").play();
+  }
+  if (e.key == "a") {
+    new Audio("../sounds/tom-2.mp3").play();
+  }
+  if (e.key == "s") {
+    new Audio("../sounds/tom-3.mp3").play();
+  }
+  if (e.key == "d") {
+    new Audio("../sounds/tom-4.mp3").play();
+  }
+  if (e.key == "j") {
+    new Audio("../sounds/snare.mp3").play();
+  }
+  if (e.key == "k") {
+    new Audio("../sounds/crash.mp3").play();
+  }
+  if (e.key == "l") {
+    new Audio("../sounds/kick-bass.mp3").play();
+  }
+});
